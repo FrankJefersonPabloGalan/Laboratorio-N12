fb69c8ead986dba1270855d60adf3ccb5194cbc9
FrankJefersonPabloGalan <149856382+FrankJefersonPabloGalan@users.noreply.github.com>
2023-11-03 23:56:11 -0500

    Add files via upload

---
+++ src/laboratorio12/Laboratorio12.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio12;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Laboratorio12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora calcu = new Calculadora();
+        calcu.setLocationRelativeTo(null);
+        calcu.setVisible(true);
+    }
+    
+}
